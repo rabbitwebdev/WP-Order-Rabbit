1b7434855dbf3b8c0ba978be459f50bb8f87452d
pipthedev <pip@rabbitwebdesign.co.uk>
2025-03-30 19:04:17 +0100

    v1.3.9

--- wp-order-rabbit.php
+++ wp-order-rabbit.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Order Rabbit
  * Description: A plugin to manage food menu items, take orders, and process payments using Stripe.
- * Version: 1.3.5
+ * Version: 1.3.9
  * Author: Your Name
  */
 
